4e8dcbb8e13ec46a9a2d619a6abea6a940997ad0
Maldreeg <127852689+Maldreeg@users.noreply.github.com>
2023-11-26 18:01:55 +0800

    improve lib

--- navigation/screens/AddCardScreen.js
+++ navigation/screens/AddCardScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  View, StyleSheet, TextInput, TouchableOpacity, Text, ImageBackground, Pressable, Image, Button } from 'react-native';
+import {  View, StyleSheet, TextInput, TouchableOpacity, Text, ImageBackground, Pressable, Image,} from 'react-native';
 
 const AddCardScreen = () => {
     return (
@@ -14,83 +14,142 @@ const AddCardScreen = () => {
                 <Text style={styles.l1}>Add Cards</Text>
             </View>
 
-            <View style={[styles.c1]}>
+            <View style={[styles.c1, styles.shadow]}>
                 <Text style={styles.l2}>ADD QUESTION</Text>
-                <Text style={styles.l3}>Question</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Question"
-                        placeholderTextColor={'rgba(31, 39, 30, 0.4)'}
-                    />
-                <Text style={styles.l4}>Answer</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Answer"
-                        placeholderTextColor={'rgba(31, 39, 30, 0.4)'}
-                    />
+                <View style={styles.c2}>
+                    <Text style={styles.l3}>Question</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Question"
+                            placeholderTextColor={'rgba(31, 39, 30, 0.4)'}
+                        />
+                    <Text style={styles.l4}>Answer</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Answer"
+                            placeholderTextColor={'rgba(31, 39, 30, 0.4)'}
+                        />
 
-                <Button title='CREATE'/>
+                    <TouchableOpacity style={[styles.createbtn, styles.shadow]}>
+                        <Text style={styles.create}>Create</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.createbtn, styles.shadow]}>
+                        <Text style={styles.create}>Done</Text>
+                    </TouchableOpacity>
+
+                </View>
+
             </View>
         </ImageBackground>
     );
 };
+
+
+
+
+
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      resizeMode: 'cover',
+        flex: 1,
+        justifyContent: 'center',
+        resizeMode: 'cover',
     },
     rectangleView: {
-      backgroundColor: "#ece3ce",
-      borderStyle: "solid",
-      borderColor: "rgba(31, 39, 30, 0)",
-      borderBottomWidth: 1,
-      width: "100%",
-      height: 90,
-      paddingTop: 40,
-      padding: 15,
-      flexDirection: 'row',
-      position: 'absolute', 
-      top: 0, 
-      zIndex: 1,
+        backgroundColor: "#ece3ce",
+        borderStyle: "solid",
+        borderColor: "rgba(31, 39, 30, 0)",
+        borderBottomWidth: 1,
+        width: "100%",
+        height: 90,
+        paddingTop: 40,
+        padding: 15,
+        flexDirection: 'row',
+        position: 'absolute', 
+        top: 0, 
+        zIndex: 1,
     },
     icon: {
-      height: 37,
-      width: 25,
+        height: 37,
+        width: 25,
     },
     back: {
-      height: 50,
-      marginRight: 20,
+        height: 50,
+        marginRight: 20,
     },
     l1: {
-      fontSize: 22,
-      color: '#00AD7C',
+        fontSize: 22,
+        color: '#00AD7C',
     },
     c1: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      alignSelf: 'center',
-      height: 400,
-      width: 300,
-      backgroundColor: "#ece3ce",
-      borderRadius: 15,
-      borderWidth: 1,
-      borderColor: 'red'
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        height: 400,
+        width: 300,
+        backgroundColor: "#ece3ce",
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: 'red'
     },
     l2: {
-      borderBottomWidth: 1,
-      borderBottomColor: 'grey'
+        fontSize: 20,
+        marginTop: 10,
+        padding: 10,
+        width: 250,
+        color: '#3A4D39',
+        fontWeight: 'bold',
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(31, 39, 30, 0.4)0',
+        textAlign: 'center',
+        position: 'absolute', 
+        top: 0, 
+        zIndex: 1,
+        
+    },
+    c2: {
+        top: 30,
+        alignItems: 'center'
+    },
+    l3:{
+        fontWeight: 'bold',
+        fontSize: 15,
+        alignSelf: 'flex-start',
+        color: '#3a4d39'
+    },
+    l4:{
+        fontWeight: 'bold',
+        fontSize: 15,
+        alignSelf: 'flex-start',
+        color: '#3a4d39'
     },
     input:{
-      width: 250,
-      height: 40,
-      borderColor: 'rgba(31, 39, 30, 0.4)',
-      borderBottomWidth: 1,
-      borderBottomColor: 'grey',
-      marginBottom: 16,
-      padding: 8,
-      color: '#3A4D39'
+        width: 250,
+        height: 40,
+        borderColor: 'rgba(31, 39, 30, 0.4)',
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        marginBottom: 16,
+        padding: 8,
+        color: '#3A4D39',
+        fontSize: 20,
     },
+    btn: {
+        margin: 10,
+        width: 200,
+        height: 50,
+        backgroundColor: 'red'
+    },
+    createbtn:{
+       width: 200,
+       height: 50,
+       alignItems: 'center',
+       justifyContent: 'center',
+       borderRadius: 50,
+       backgroundColor: '#52d681',
+       margin: 5
+    },
+
 
   
     // Shadow Effect ;3
--- navigation/screens/LibrariesScreen.js
+++ navigation/screens/LibrariesScreen.js
@@ -1,13 +1,104 @@
 import * as React from 'react';
-import { View, StyleSheet, TextInput, TouchableOpacity, Text, ImageBackground } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View, TouchableOpacity, ImageBackground, ScrollView} from 'react-native';
+
 
 export default function LibrariesScreen({ navigation }) {
+
+    const flatlistdata = [
+        {
+            id: 1,
+            title: 'Javascript Coding',
+            cardno: '21 CARDS'
+        },
+        {
+            id: 2,
+            title: 'Facts',
+            cardno: '30 CARDS'
+        },
+        {
+            id: 3,
+            title: 'Humanities for Dummies',
+            cardno: '19 CARDS'
+        },
+        {
+            id: 4,
+            title: 'Integration for CS',
+            cardno: '42 CARDS'
+        },
+        {
+            id: 5,
+            title: 'Reviewer',
+            cardno: '11 CARDS'
+        },
+        {
+            id: 6,
+            title: "OnlyFriend's Location",
+            cardno: '12 CARDS'
+        },
+
+
+    ];
+    
     return (
-        
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Libraries Screen</Text>
+      <View >
+        <SafeAreaView/>
+        <View style={styles.c1}>
+            <Text style={styles.label}>Created</Text>
+            <FlatList style={styles.c2} data={flatlistdata} renderItem={({item})=><View>
+                <View style={styles.createdContainer}>
+                    <TouchableOpacity style={styles.cardbtn}>
+                        <Text>{item.title}</Text>
+                        <Text>{item.cardno}</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>}/>
         </View>
+        <View style={styles.c1}>
+            <Text style={styles.label}>Favorites</Text>
+            <FlatList style={styles.c2} data={flatlistdata} renderItem={({item})=><View>
+                <View style={styles.createdContainer}>
+                    <TouchableOpacity style={styles.cardbtn}>
+                        <Text>{item.title}</Text>
+                        <Text>{item.cardno}</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>}/>
+        </View>
+        
+      </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+    cardbtn:{
+        backgroundColor: 'red',
+        width: '90%',
+        height: 60,
+        margin: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15
+        
+    },
+    createdContainer:{
+        alignItems: 'center',
+        flex: 1,
+        padding: 5,
+    },
+    label:{
+        fontSize: 20
+    },
+    c1: {
+        padding: 10,
+        marginBottom: 25,
+        backgroundColor: 'green'
+    },
+    c2: {
+        height: '40%',
+        width: '100%',
+        paddingVertical:20,
+        backgroundColor: 'blue'
+    }
+    
+});
